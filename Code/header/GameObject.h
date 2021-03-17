eac3edc8c60bd90f5c3facb166e9c86e6aa27423
hippo's dell desktop <hippogyz@hotmail.com>
2021-03-17 13:46:45 +0800

    component

--- Code/header/GameObject.h
+++ Code/header/GameObject.h
@@ -11,12 +11,21 @@ public: // member
 	bool is_active;
 
 public: // method
-	void update(float delta);
+	void uniform_update(float delta);
+	virtual void start();
+	virtual void update(float delta);
 	
 	template < typename ComponentName > 
-		ComponentName& GetComponent();
+		ComponentName& getComponent();
 	template < typename ComponentName, typename... Args >
-		void AddComponent(Args&&... args);
+		void addComponent(Args&&... args);
 	template < typename ComponentName >
-		bool RemoveComponent();
+		void removeComponent();
+
+private: // member
+	std::vector< unsigned int > removing_component;
+	std::vector< std::unique_ptr<Component> > component_buffer;
+	
+private: // method
+	void arrangeComponent();
 };
